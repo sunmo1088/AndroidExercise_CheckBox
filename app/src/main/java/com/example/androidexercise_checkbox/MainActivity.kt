5a5a7d38a7b7268328c89a2fdc1f8564c447ba25
Sun Mo <sun.mo1088@gmail.com>
2023-04-28 13:53:36 -0600

    Radio button and check box exercise

--- app/src/main/java/com/example/androidexercise_checkbox/MainActivity.kt
+++ app/src/main/java/com/example/androidexercise_checkbox/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.androidexercise_checkbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import com.example.androidexercise_checkbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOrder.setOnClickListener {
+            val checkedMeatButtonId = binding.rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMeatButtonId)
+            val cheese = binding.cbCheese.isChecked
+            val onion = binding.cbOnions.isChecked
+            val salad = binding.cbSalad.isChecked
+            val orderString = "You ordered a burger with:\n" +
+                    "${meat.text}" +
+                    (if (cheese) "\nCheese" else "") +
+                    (if (onion) "\nOnion" else "") +
+                    (if (salad) "\nSalad" else "")
+            binding.tvOrder.text = orderString
+        }
+
+
     }
 }
